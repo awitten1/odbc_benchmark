ca4268dbb569f65d1f9b6cd85e14ec6badcb8ed8
Andrew Witten <awitten007@gmail.com>
2025-12-11 20:34:13 -0500

    setting pg options now

--- launch-postgres.sh
+++ launch-postgres.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #
-# For this to work, I had to run sudo chmod a+w /var/run/postgresql.
 # After running this script connect to postgres with `psql -d postgres`.
 #
 
@@ -21,12 +20,27 @@ pg_versions=(/usr/lib/postgresql/*)
 
 pg_version=${pg_versions[-1]}
 
+set_pg_option() {
+    key=$1
+    value=$2
+    pg_conf="$3/postgresql.conf"
 
-if [ $1 = "start" ]; then
+    sed -i "/$1/d" $pg_conf
+    echo "$key = $value" >> $pg_conf
+}
+
+
+if [[ $1 = "start" || $1 = "init" ]]; then
     if [ ! -f data_dir/PG_VERSION ]; then
         $pg_version/bin/pg_ctl init -D data_dir
+        set_pg_option 'unix_socket_directories' "'/tmp'" "data_dir"
+        set_pg_option 'logging_collector' "true" "data_dir"
+        set_pg_option 'log_destination' "'csvlog,jsonlog'" "data_dir"
+        set_pg_option 'log_directory' "'logs'" "data_dir"
     fi
-    $pg_version/bin/pg_ctl start -D data_dir
+    if [ $1 = "start" ]; then
+        $pg_version/bin/pg_ctl start -D data_dir
+    fi
 elif [ $1 = "stop" ]; then
     $pg_version/bin/pg_ctl stop -D data_dir -m fast
 fi

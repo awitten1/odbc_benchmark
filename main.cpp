8227c97baacc5043dc1602549b51cbccf30ea5cd
Andrew Witten <awitten007@gmail.com>
2025-12-17 16:44:04 -0500

    using async api

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ struct Args {
     std::string host;
     std::string dbname;
     std::string user;
+    bool use_async_libpq = false;
 };
 
 Args parse_cli_params(int argc, char** argv) {
@@ -26,22 +27,24 @@ Args parse_cli_params(int argc, char** argv) {
         std::string arg(argv[i]);
         auto pos = arg.find("=");
         if (pos == std::string::npos) {
-            std::cerr << "arguments must be key-value pairs formatted as <key>=<value>" 
+            std::cerr << "arguments must be key-value pairs formatted as <key>=<value>"
                 << std::endl;
             exit(EXIT_FAILURE);
         }
         std::string key(arg.begin(), arg.begin() + pos);
         std::string value(arg.begin() + pos + 1, arg.end());
-        if (key == "password") {
+        if (key == "--password") {
             args.password = value;
-        } else if (key == "host") {
+        } else if (key == "--host") {
             args.host = value;
-        } else if (key == "port") {
+        } else if (key == "--port") {
             args.port = std::stoi(value);
-        } else if (key == "dbname") {
+        } else if (key == "--dbname") {
             args.dbname = value;
-        } else if (key == "user") {
+        } else if (key == "--user") {
             args.user = value;
+        } else if (key == "--use_async") {
+            args.use_async_libpq = value == "true";
         } else {
             std::cerr << "unknown key " << key << std::endl;
             exit(EXIT_FAILURE);
@@ -65,17 +68,8 @@ PGconn* connect(Args args) {
     return c;
 }
 
-int main(int argc, char** argv) {
-    Args args = parse_cli_params(argc, argv);
-    auto free_pg_conn = [](PGconn* conn) {
-        PQfinish(conn);
-    };
-    std::unique_ptr<PGconn,decltype(free_pg_conn)> conn(
-        connect(args),free_pg_conn);
 
-    const char* query = "select * from x;";
-    PGresult* result = PQexec(conn.get(), query);
-    std::cout << "query " << query << " returned " << PQntuples(result) << " tuples" << std::endl;
+void print_pg_result(PGresult* result) {
     int nrows = PQntuples(result);
     int ncols = PQnfields(result);
 
@@ -86,7 +80,49 @@ int main(int argc, char** argv) {
         }
         printf("\n");
     }
+}
 
+void use_synchronous_libpq(PGconn* conn, const char* query) {
+    PGresult* result = PQexec(conn, query);
+    print_pg_result(result);
     PQclear(result);
+}
+
+void use_async_libpq(PGconn* conn, const char* query, int chunk_size = 1000) {
+    int ret = PQsendQuery(conn, query);
+    if (ret == 0) {
+        fprintf(stderr, "failed to run sendQuery %s", PQerrorMessage(conn));
+        return;
+    }
+
+    PQsetChunkedRowsMode(conn, chunk_size);
+
+    for (;;) {
+        PGresult* result = PQgetResult(conn);
+        if (!result) {
+            break;
+        }
+        print_pg_result(result);
+        PQclear(result);
+    }
+
+}
+
+int main(int argc, char** argv) {
+    Args args = parse_cli_params(argc, argv);
+    auto free_pg_conn = [](PGconn* conn) {
+        PQfinish(conn);
+    };
+    std::unique_ptr<PGconn,decltype(free_pg_conn)> conn(
+        connect(args),free_pg_conn);
+
+    const char* query = "select * from x;";
+    if (!args.use_async_libpq) {
+        use_synchronous_libpq(conn.get(), query);
+    } else {
+        use_async_libpq(conn.get(), query);
+    }
+
+
     return 0;
 }
